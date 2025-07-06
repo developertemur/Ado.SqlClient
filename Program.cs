0b2ba4363f6bdfa2efde73ee20a1dbddbc8c739c
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2025-07-06 18:05:58 +0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Sql server connection example with windows authentication
+// This code connects to a SQL Server database and retrieves data from a table named 'users'
+
+using Microsoft.Data.SqlClient;
+var connectionString = "Server=Temur;Database=Test1;Integrated Security=True;TrustServerCertificate=True";
+using (var connection = new SqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        SqlCommand command = new SqlCommand("SELECT * from users", connection);
+        using(var reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine($"User: {reader["name"]}, age: {reader["age"]}");
+            }
+        }
+        Console.WriteLine("Connection to SQL Server established successfully.");
+    }
+    catch (SqlException ex)
+    {
+        Console.WriteLine($"SQL Error: {ex.Message}");
+    }
+}
